61437cf5933453b7db8220165df4938e5194502f
MasseRaphael <38262822+MasseRaphael@users.noreply.github.com>
2020-02-07 15:56:54 +0100

    ajout logique js de l'algorythme

---
+++ testCard.js
@@ -0,0 +1,18 @@
+test 
+
+
+<script>
+    var nbJoueur = 2;
+
+    var joueurActuel = 0;
+
+    var carte = 0;
+
+    var tabCarteJoueur = [];
+
+    tabCarteJoueur[0] = [1,2,3];
+
+    console.log(tabCarteJoueur);
+    
+
+</script>
